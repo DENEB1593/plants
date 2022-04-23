4ca4dc79eca8e01d4280c29f220704422d3ab309
leekangwoon <deneb1593@gmail.com>
2022-04-24 00:54:11 +0900

    코드 정리

--- src/main/java/io/sinsabridge/plants/domain/plant/PlantApiController.java
+++ src/main/java/io/sinsabridge/plants/domain/plant/PlantApiController.java
@@ -11,22 +11,6 @@ import java.util.List;
 @RequestMapping("/plants")
 public class PlantApiController {
     /**
-     * 식물 유형 코드 조회
-     */
-    @GetMapping("/types")
-    public List<PlantType> types() {
-        return Collections.unmodifiableList(Arrays.asList(PlantType.values()));
-    }
-
-    /**
-     * 식물유형 별 식물 조회
-     */
-    @GetMapping("/{plantType}")
-    public List<Plant> getPlantsByType(@PathVariable("plantType") PlantType plantType) {
-        return null;
-    }
-
-    /**
      * 식물 조회
      */
     @GetMapping
@@ -41,4 +25,35 @@ public class PlantApiController {
     public void registPlant(@RequestBody PlantDto plantDto) {
 
     }
+
+    /**
+     * 식물 정보 수정
+     */
+    @PutMapping
+    public void updatePlant(@RequestBody PlantDto plantDto) {
+
+    }
+
+    /**
+     * 식물 삭제
+     */
+    @DeleteMapping
+    public void deletePlant(Long plantNo) {
+    }
+
+    /**
+     * 식물유형 별 식물 조회
+     */
+    @GetMapping("/{plantType}")
+    public List<Plant> getPlantsByType(@PathVariable("plantType") PlantType plantType) {
+        return null;
+    }
+    /**
+     * 식물 유형 코드 조회
+     */
+    @GetMapping("/types")
+    public List<PlantType> types() {
+        return Collections.unmodifiableList(Arrays.asList(PlantType.values()));
+    }
+
 }
