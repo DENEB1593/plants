fdb88872fdc76f06ac9e27dbfaad708371a0c3f4
leekangwoon <deneb1593@gmail.com>
2022-04-25 23:53:35 +0900

    문자인증번호생성기 추가

--- src/main/java/io/sinsabridge/plants/infra/SmsSendManager.java
+++ src/main/java/io/sinsabridge/plants/infra/SmsSendManager.java
@@ -2,14 +2,27 @@ package io.sinsabridge.plants.infra;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
 @Component(value = "smsSendManager")
 public class SmsSendManager implements SendManger {
+    Supplier<String> verificationCodeGenerator = () -> {
+        return new Random().ints(0, 10)
+                .limit(6)
+                .boxed()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    };
+
     /**
      * 문자 발송을 정의한다
      */
     @Override
     public void send(String content) {
         // TODO: 2022-04-25 : 문자 발송 구현
+        verificationCodeGenerator.get();
     }
 
 }
